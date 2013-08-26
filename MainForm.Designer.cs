7b5b098fc8e34d22092383a514ca9b91d9ab01b5
vatslav <vatslav.startup@yandex.ru>
2013-08-26 14:00:51 +0400

    разработка ядра, добавление картинки

---
+++ MainForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace watercolor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bOpen = new System.Windows.Forms.Button();
+            this.bApply = new System.Windows.Forms.Button();
+            this.bSafe = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bOpen
+            // 
+            this.bOpen.Location = new System.Drawing.Point(12, 12);
+            this.bOpen.Name = "bOpen";
+            this.bOpen.Size = new System.Drawing.Size(91, 61);
+            this.bOpen.TabIndex = 0;
+            this.bOpen.Text = "выбрать изображение";
+            this.bOpen.UseVisualStyleBackColor = true;
+            // 
+            // bApply
+            // 
+            this.bApply.Location = new System.Drawing.Point(109, 12);
+            this.bApply.Name = "bApply";
+            this.bApply.Size = new System.Drawing.Size(91, 61);
+            this.bApply.TabIndex = 1;
+            this.bApply.Text = "применить фильтр";
+            this.bApply.UseVisualStyleBackColor = true;
+            // 
+            // bSafe
+            // 
+            this.bSafe.Location = new System.Drawing.Point(206, 12);
+            this.bSafe.Name = "bSafe";
+            this.bSafe.Size = new System.Drawing.Size(91, 61);
+            this.bSafe.TabIndex = 2;
+            this.bSafe.Text = "сохранить изображение";
+            this.bSafe.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(318, 94);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(284, 164);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 263);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.bSafe);
+            this.Controls.Add(this.bApply);
+            this.Controls.Add(this.bOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bOpen;
+        private System.Windows.Forms.Button bApply;
+        private System.Windows.Forms.Button bSafe;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ core.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace watercolor
+{   
+    class core
+    {
+        public Bitmap bitmap;
+        public Graphics g;
+        Image img;
+        PictureBox mainCanvas;
+        core(PictureBox mainCanvas) 
+        { 
+            this.mainCanvas = mainCanvas;
+        
+        }
+        public void opemFile(String path)
+        {
+               if (bitmap != null)
+               {
+                  bitmap.Dispose();
+               }
+            img = Image.FromFile(path);
+            bitmap = new Bitmap(mainCanvas.Height, mainCanvas.Width);
+            g = Graphics.FromImage(img);
+            g.
+        }
+
+    }
+}
