6fc22d35c28153a5a1633b600ab414243b9677ba
vatslav <vatslav.startup@yandex.ru>
2013-09-04 23:35:16 +0400

    косметические изменения

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,10 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.bOpen = new System.Windows.Forms.Button();
-            this.bApply = new System.Windows.Forms.Button();
-            this.bSafe = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.brightTrackBar = new System.Windows.Forms.TrackBar();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
@@ -41,58 +37,23 @@
             this.label1 = new System.Windows.Forms.Label();
             this.brightLabel = new System.Windows.Forms.Label();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.bSafe = new System.Windows.Forms.Button();
+            this.bApply = new System.Windows.Forms.Button();
+            this.bOpen = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.brightTrackBar)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox4.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // bOpen
-            // 
-            this.bOpen.Location = new System.Drawing.Point(200, 19);
-            this.bOpen.Name = "bOpen";
-            this.bOpen.Size = new System.Drawing.Size(91, 61);
-            this.bOpen.TabIndex = 0;
-            this.bOpen.Text = "выбрать изображение";
-            this.bOpen.UseVisualStyleBackColor = true;
-            this.bOpen.Click += new System.EventHandler(this.bOpen_Click);
-            // 
-            // bApply
-            // 
-            this.bApply.Location = new System.Drawing.Point(6, 19);
-            this.bApply.Name = "bApply";
-            this.bApply.Size = new System.Drawing.Size(91, 61);
-            this.bApply.TabIndex = 1;
-            this.bApply.Text = "применить фильтр";
-            this.bApply.UseVisualStyleBackColor = true;
-            this.bApply.Click += new System.EventHandler(this.bApply_Click);
-            // 
-            // bSafe
-            // 
-            this.bSafe.Location = new System.Drawing.Point(103, 19);
-            this.bSafe.Name = "bSafe";
-            this.bSafe.Size = new System.Drawing.Size(91, 61);
-            this.bSafe.TabIndex = 2;
-            this.bSafe.Text = "сохранить изображение";
-            this.bSafe.UseVisualStyleBackColor = true;
-            this.bSafe.Click += new System.EventHandler(this.bSafe_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(13, 19);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(412, 258);
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // brightTrackBar
             // 
             this.brightTrackBar.LargeChange = 1;
-            this.brightTrackBar.Location = new System.Drawing.Point(6, 32);
+            this.brightTrackBar.Location = new System.Drawing.Point(9, 48);
             this.brightTrackBar.Maximum = 256;
             this.brightTrackBar.Name = "brightTrackBar";
             this.brightTrackBar.Size = new System.Drawing.Size(309, 42);
@@ -107,21 +68,23 @@
             this.groupBox1.Controls.Add(this.groupBox4);
             this.groupBox1.Controls.Add(this.groupBox2);
             this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.groupBox1.Location = new System.Drawing.Point(443, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(336, 292);
+            this.groupBox1.Size = new System.Drawing.Size(418, 347);
             this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Управление изображением";
             // 
             // groupBox4
             // 
+            this.groupBox4.Controls.Add(this.bSafe);
             this.groupBox4.Controls.Add(this.bApply);
             this.groupBox4.Controls.Add(this.bOpen);
-            this.groupBox4.Controls.Add(this.bSafe);
+            this.groupBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.groupBox4.Location = new System.Drawing.Point(6, 20);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(318, 110);
+            this.groupBox4.Size = new System.Drawing.Size(397, 137);
             this.groupBox4.TabIndex = 12;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Быстрый доступ";
@@ -129,9 +92,10 @@
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.comboBox1);
-            this.groupBox2.Location = new System.Drawing.Point(6, 136);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Location = new System.Drawing.Point(6, 163);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(324, 62);
+            this.groupBox2.Size = new System.Drawing.Size(397, 76);
             this.groupBox2.TabIndex = 10;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Тип фильтра";
@@ -141,7 +105,7 @@
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(6, 19);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(278, 21);
+            this.comboBox1.Size = new System.Drawing.Size(278, 24);
             this.comboBox1.TabIndex = 9;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -150,9 +114,10 @@
             this.groupBox3.Controls.Add(this.brightTrackBar);
             this.groupBox3.Controls.Add(this.label1);
             this.groupBox3.Controls.Add(this.brightLabel);
-            this.groupBox3.Location = new System.Drawing.Point(6, 204);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox3.Location = new System.Drawing.Point(12, 245);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(324, 82);
+            this.groupBox3.Size = new System.Drawing.Size(391, 96);
             this.groupBox3.TabIndex = 11;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Яркость изображения";
@@ -160,41 +125,89 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Location = new System.Drawing.Point(11, 29);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.Size = new System.Drawing.Size(135, 16);
             this.label1.TabIndex = 7;
             this.label1.Text = "Текущее значение:";
             // 
             // brightLabel
             // 
             this.brightLabel.AutoSize = true;
-            this.brightLabel.Location = new System.Drawing.Point(117, 16);
+            this.brightLabel.Location = new System.Drawing.Point(152, 29);
             this.brightLabel.Name = "brightLabel";
-            this.brightLabel.Size = new System.Drawing.Size(25, 13);
+            this.brightLabel.Size = new System.Drawing.Size(29, 16);
             this.brightLabel.TabIndex = 8;
             this.brightLabel.Text = "128";
             // 
             // groupBox5
             // 
             this.groupBox5.Controls.Add(this.pictureBox1);
+            this.groupBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.groupBox5.Location = new System.Drawing.Point(6, 13);
             this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(431, 291);
+            this.groupBox5.Size = new System.Drawing.Size(431, 346);
             this.groupBox5.TabIndex = 8;
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "Эскиз";
+            // 
+            // bSafe
+            // 
+            this.bSafe.BackgroundImage = global::watercolor.Properties.Resources.Gift_Artdesigner;
+            this.bSafe.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bSafe.Location = new System.Drawing.Point(276, 21);
+            this.bSafe.Name = "bSafe";
+            this.bSafe.Size = new System.Drawing.Size(115, 101);
+            this.bSafe.TabIndex = 2;
+            this.bSafe.Text = "сохранить изображение";
+            this.bSafe.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.bSafe.UseVisualStyleBackColor = true;
+            this.bSafe.Click += new System.EventHandler(this.bSafe_Click);
+            // 
+            // bApply
+            // 
+            this.bApply.BackgroundImage = global::watercolor.Properties.Resources.Art_Artdesigner;
+            this.bApply.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bApply.Location = new System.Drawing.Point(137, 21);
+            this.bApply.Name = "bApply";
+            this.bApply.Size = new System.Drawing.Size(115, 101);
+            this.bApply.TabIndex = 1;
+            this.bApply.Text = "применить фильтр";
+            this.bApply.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.bApply.UseVisualStyleBackColor = true;
+            this.bApply.Click += new System.EventHandler(this.bApply_Click);
+            // 
+            // bOpen
+            // 
+            this.bOpen.BackgroundImage = global::watercolor.Properties.Resources.Open;
+            this.bOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bOpen.Location = new System.Drawing.Point(6, 21);
+            this.bOpen.Name = "bOpen";
+            this.bOpen.Size = new System.Drawing.Size(115, 101);
+            this.bOpen.TabIndex = 0;
+            this.bOpen.Text = "выбрать изображение";
+            this.bOpen.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.bOpen.UseVisualStyleBackColor = true;
+            this.bOpen.Click += new System.EventHandler(this.bOpen_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(412, 315);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(792, 319);
+            this.ClientSize = new System.Drawing.Size(904, 397);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox5);
             this.Name = "MainForm";
-            this.Text = "Акварелизация";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Text = "Сундучок с фильтрами v.1.5";
             ((System.ComponentModel.ISupportInitialize)(this.brightTrackBar)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox4.ResumeLayout(false);
@@ -202,6 +215,7 @@
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
             this.groupBox5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
